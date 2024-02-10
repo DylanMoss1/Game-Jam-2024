66f171ee88736d0e13febd61a1ffc65693ce91ca
Dylan Moss <dylan.moss2001@gmail.com>
2024-02-10 01:52:30 +0000

    Refactor screen_render

--- src/screen_render/object_properties/renderable.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-
-
-class Renderable(ABC):
-
-  # Please override when implementing this class
-  def render(self, render_screen, screen_width, screen_height) -> None:
-    pass
