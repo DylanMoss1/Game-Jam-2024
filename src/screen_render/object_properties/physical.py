0e4123929b6f5066c0074c002772dfd1c23220a9
Dylan Moss <dylan.moss2001@gmail.com>
2024-02-10 01:52:30 +0000

    Refactor screen_render

--- src/screen_render/object_properties/physical.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-
-
-class Physical(ABC):
-
-  # Please override when implementing this class
-  def setup_physics(self, physics_engine) -> None:
-    pass
--- src/screen_render/screen_render.py
+++ src/screen_render/screen_render.py
@@ -1,147 +1,99 @@
-from objects.line import Line
-from object_collections.renderable_objects_collection import RenderableObjectsCollection
-from object_collections.physical_object_collection import PhysicalObjectsCollection
 import pygame
 from pygame.locals import *
 import pymunk
 import pymunk.pygame_util
+import random
 
-# --- Initialise PyGame ---
+
+# --- Initialise PyGame (Rendering Engine) ---
 
 pygame.init()
 render_clock = pygame.time.Clock()
 
-render_screen = pygame.display.set_mode((400, 300), RESIZABLE)
-
-is_main_game_loop_running = True
+render_screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 render_font = pygame.font.SysFont(None, 48)
 
-# --- Initialise PyMunk ---
 
-physics_gravity_scale = -900.0
-physics_wind_scale = 0.0
+# --- Initialise PyMunk (Physics Engine) ---
 
-physics_engine = pymunk.Space()
-physics_engine.gravity = (physics_gravity_scale, physics_wind_scale)
-
-# --- Add New Objects To Scene ---
-
-all_renderable_objects = RenderableObjectsCollection()
-all_physical_objects = PhysicalObjectsCollection()
+physics_space = pymunk.Space()
+physics_space.gravity = (0.0, 900.0)
 
 
-def setup_object(object, is_renderable, is_physical):
-  if is_renderable:
-    all_renderable_objects.add(object)
+# --- Add Objects To Scene ---
 
-  if is_physical:
-    object.setup_physics(physics_engine)
-    all_physical_objects.add(object)
-
-
-line = Line(start_position=(0.1, 0.9), end_position=(0.9, 0.9))
-setup_object(line, is_renderable=True, is_physical=True)
-
-# --- Main Gameplay Loop ---
-
-while is_main_game_loop_running:
-
-  current_screen_width = render_screen.get_width()
-  current_screen_height = render_screen.get_height()
-
-  render_clock.tick(60)
-  for pygame_user_input_event in pygame.event.get():
-    if pygame_user_input_event.type == pygame.QUIT:
-      is_main_game_loop_running = False
-    elif pygame_user_input_event.type == VIDEORESIZE:
-      all_physical_objects.setup_physics_on_all(physics_engine)
-
-  render_screen.fill((200, 200, 200))
-
-  all_renderable_objects.render_all(render_screen, current_screen_width, current_screen_height)
-
-  physics_engine.step(1/60.0)
-  pygame.display.update()
-
-pygame.quit()
+BALL_MASS = 1
+BALL_RADIUS = 14
+BALL_ELASTICITY = 1.0
+BALL_FRICTION = 1.0
 
 
-# rad = 14
-# ball_elasticity = 0.8
-# friction = 1.0
-# circles = []
+def add_ball(physics_space, position) -> pymunk.Circle:
 
-# def create_circle(position):
-#     mass = 1
-#     inertia = pymunk.moment_for_circle(mass, 0, rad)
-#     body = pymunk.Body(mass, inertia)
-#     body.position = position
-#     shape = pymunk.Circle(body,rad)
-#     shape.elasticity = ball_elasticity
-#     shape.friction = friction
-#     space.add(body,shape)
+  inertia = pymunk.moment_for_circle(BALL_MASS, 0, BALL_RADIUS, (0, 0))
 
-#     return shape
+  body = pymunk.Body(BALL_MASS, inertia)
+  body.position = position
 
-# def create_line(p1,p2):
-#     static_body = space.static_body
-#     static_body.position = (400, 600)
-#     line_shape = pymunk.Segment(static_body, p1, p2, 15)
-#     line_shape.elasticity = 1.0
-#     line_shape.color = (100, 200, 0)
-#     space.add(line_shape)
-#     return line_shape
+  shape = pymunk.Circle(body, BALL_RADIUS)
+  shape.elasticity = BALL_ELASTICITY
+  shape.friction = BALL_FRICTION
 
-# create_line((400,-500),(400,0))
-# create_line((-400,-500),(-400,0))
+  physics_space.add(body, shape)
+
+  return shape
 
 
-# while running:
-#     clock.tick(60)
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#         elif event.type == pygame.MOUSEBUTTONDOWN:
-#             originalMousePos = pygame.mouse.get_pos()
-#             realPos = pymunk.pygame_util.to_pygame(originalMousePos, screen)
-#             newCircle = create_circle(realPos)
-#             circles.append(newCircle)
-#             print(len(circles))
+def add_line(physics_space, start_position, end_position) -> pymunk.Segment:
+  body = pymunk.Body(body_type=pymunk.Body.STATIC)
 
-#         if event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_RIGHT:
-#                 wind+=50
-#             if event.key == pygame.K_LEFT:
-#                 wind-=50
-#             if event.key == pygame.K_UP:
-#                 gravity+=50
-#             if event.key == pygame.K_DOWN:
-#                 gravity-=50
-#             if event.key == pygame.K_l:
-#                 rad = 1
-#                 friction = 0.1
+  shape = pymunk.Segment(body, start_position, end_position, radius=1)
 
-#     screen.fill((50,50,50))
+  shape.elasticity = 0.0
+  shape.friction = 0.0
 
-#     for circle in circles:
-#         circlePosition = int(circle.body.position.x), 600-int(circle.body.position.y)
-#         pygame.draw.circle(screen, (35, 88, 150), circlePosition, int(circle.radius), 0)
+  physics_space.add(body, shape)
+
+  return shape
 
 
-#     circleCount = font.render(str(len(circles)), 1, (255, 0, 0))
-#     screen.blit(circleCount, (30, 10))
-
-#     currentFPS = font.render(str(int(clock.get_fps())), 1, (255, 0, 0))
-#     screen.blit(currentFPS, (30, 40))
+def add_lines_from_position_list(positions):
+  for start_position, end_position in positions:
+    add_line(physics_space, start_position, end_position)
 
 
-#     pygame.draw.line(screen, line.color,(0,500),(800, 500), 30)
-#     pygame.draw.line(screen, line.color,(0,500),(0,0),30)
-#     pygame.draw.line(screen, line.color,(800,500),(800,0),30)
+def main():
 
-#     space.gravity = (wind, gravity)
-#     space.step(1/60.0)
-#     pygame.display.update()
+  is_main_game_loop_running = True
 
-# pygame.quit()
+  # Lists must be kept alive for balls to render
+  balls = [add_ball(physics_space, (120, 50))]
+  lines = [add_lines_from_position_list([
+      ((100, 100), (200, 200)),
+      ((200, 200), (300, 300)),
+      ((300, 300), (400, 400)),
+      ((400, 400), (500, 500)),
+  ])]
+
+  draw_options = pymunk.pygame_util.DrawOptions(render_screen)
+
+  while is_main_game_loop_running:
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        is_main_game_loop_running = False
+      elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        is_main_game_loop_running = False
+
+    render_screen.fill((255, 255, 255))
+    physics_space.debug_draw(draw_options)
+
+    pygame.display.flip()
+
+    render_clock.tick(60)
+    physics_space.step(1 / 60.0)
+
+
+if __name__ == "__main__":
+  main()
